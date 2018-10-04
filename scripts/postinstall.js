7e291d9fb2eb86d69e26baf0a478da75b0b741a0
Taylor Dawson <taylorjdawson@gmail.com>
2018-10-04 16:52:22 -0700

    added log for user friendlyness

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -12,5 +12,6 @@ let addUploadFile = function() {
     if (err) throw err
     if (!files.length > 0) throw new Error('migrations folder is empty')
     fs.writeFileSync(`migrations/${files.length + 2}_abi_analytics`, 'import uploadModule from \'web3data-deploy\'\nmodule.exports = function(network, accounts) {\n\tuploadModule(network, accounts)\n}', 'utf-8')
+    console.log(`Added \'${files.length + 2}_abi_analytics\' to \'migrations\' directory`)
   })
 }
