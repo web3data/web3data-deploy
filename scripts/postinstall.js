6f7b744b912744cf1c5902be2d427f097e684198
Taylor Dawson <taylorjdawson@gmail.com>
2018-10-08 14:01:28 -0700

    [W.I.P.] Testing

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -12,6 +12,7 @@ let addUploadFile = function() {
     if (err) throw err
     if (!files.length > 0) throw new Error('migrations folder is empty')
     const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${files.length + 2}_abi_analytics.js`)
+    console.log('ABI_ANALYTICS_PATH', ABI_ANALYTICS_PATH)
     fs.writeFileSync(ABI_ANALYTICS_PATH, 'const uploadModule = require(\'web3data-deploy\')\nmodule.exports = function(network, accounts) {\n\tuploadModule(network, accounts)\n}', 'utf-8')
     console.log(`Added \'${files.length + 2}_abi_analytics\' to \'migrations\' directory`)
   })
