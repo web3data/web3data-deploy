618718dfd67394b51d704ac3115d5ccd7785dd66
Taylor Dawson <taylorjdawson@gmail.com>
2018-10-10 15:23:07 -0700

    Major Refactor:  - Added usage of path and url (per best practices)  - Added commenting  - Added additional error checking  - Added More user messages

--- index.js
+++ index.js
@@ -1,77 +1,135 @@
-let fs = require('fs')
-let url = require('url')
-let path = require('path')
-let FormData = require('form-data')
-let dotenv = require('dotenv')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+const FormData = require('form-data')
+const dotenv = require('dotenv')
+
 dotenv.load()
 
-// const prodUrl = url.parse('https://contract-service.amberdata.io/api/v1/upload')
-// const devUrl = url.parse('http://localhost:1234/api/v1/upload')
-const prodTestUrl = 'https://contract-service.amberdata.io/api/v1/upload'
-const mainUrl = url.parse(process.env.UPLOADENDPOINT || prodTestUrl)
+const prodUrl = url.parse('https://contract-service.amberdata.io/api/v1/upload')
+const devUrl = url.parse('http://localhost:1234/api/v1/upload')
 const mainPath = process.env.NODE_ENV === 'development' ? devUrl : prodUrl
 const headers = {'x-amberdata-api-key': process.env.AMBERDATA_API_KEY || ''}
 
+// The standard path to the contract apis per truffle specs
 const DEFAULT_BUILD_DIR = path.join(__dirname, '/../../', 'build/contracts/')
 
-const blockchainIds = {
-  1: '1c9c969065fcd1cf', // Ethereum main-net
-  4: '1b3f7a72b3e99c13' // Rinkeby Testnet
-}
+// An Object containing amberdata specfic values for use with our api
 const blockchainIds = {
   1: {
     blockchainId: '1c9c969065fcd1cf',
     slug: 'ethereum-mainnet'
-  }, // Ethereum main-net
-  4: '1b3f7a72b3e99c13' // Rinkeby Testnet
+  },
+  4: {
+    blockchainId: '1b3f7a72b3e99c13',
+    slug: 'ethereum-rinkeby'
+  }
 }
 
-const getMeta = function(networks, network_id, account) {
+// Ethereum JSON RPC network_ids for user help message
+const ETHEREUM_NETWORKS = {
+  1: 'Ethereum Mainnet',
+  3: 'Ropsten Testnet',
+  4: 'Rinkeby Testnet',
+  42: 'Kovan Testnet'
+}
+
+/**
+ * @desc getMeta() returns an object containing the
+ * metadata of the contract to upload with the abi
+ * @param { Object } networks contains details of the deployed contract
+ * @param { Object } networkId the JSON RPC network_id of the
+ *                   network that the contract was deployed to
+ * @param { Object } account the deployer account address
+ * @return { Object } the metadata of the contract
+ */
+const getMeta = function(networks, networkId, account) {
   return {
     contractAddress: networks.address,
     walletAddress: account,
     transactionHash: networks.transactionHash,
-    blockchainId: blockchainIds[network_id]
+    blockchainId: blockchainIds[networkId].blockchainId,
+    slug: blockchainIds[networkId].slug
   }
 }
 
-const uploadFile = function(file, payload) {
+/**
+ * @desc uploadAbi() uploads a multipart format of files
+ * @param { String } file the path of the file to upload
+ * @param { Object } payload the metadata to upload with the contract abi
+ */
+const uploadAbi = function(file, payload) {
   const form = new FormData()
   form.append('uploadedFiles', fs.createReadStream(file))
   form.append('contractAddress', payload.contractAddress)
   form.append('transactionHash', payload.transactionHash)
   form.append('walletAddress', payload.walletAddress)
   form.append('blockchainId', payload.blockchainId)
-  const options = Object.assign({}, mainPath, { headers })
-  form.submit(
-    options,
-    (err, res) => {
+  const options = Object.assign({}, mainPath, {headers})
+  form.submit(options, (err, res) => {
+    if (err) {
       console.log(err, res.statusCode)
-
-      console.log('LINK TO THE CONTRACT IN AMBERDATA')
-      console.log('https://ethereum-rinkeby.amberdata.io/addresses/HASHHERE/management')
+    } else {
+      console.log(
+        `[ Uploader ] \u001B[32mINFO:\u001B[0m View your contract at: \u001B[36mhttps://${
+          payload.slug
+        }.amberdata.io/addresses/${payload.contractAddress}/management\u001B[0m`
+      )
     }
-  )
+  })
 }
 
+/**
+ * @desc isEmpty() takes an object and
+ * evaluates whether an object is empty or not
+ * @param { Object } obj the object that's tested for emptiness
+ * @return { Boolean } true if empty and false otherwise
+ */
 const isEmpty = function(obj) {
   return Object.keys(obj).length === 0 && obj.constructor === Object
 }
 
+/**
+ * @desc Uploads contract abi to amberdata servers
+ * @param { Object } deployer the truffle deployer object
+ * @param { String } network name of the network as defined in truffle.js
+ * @param { Array } accounts the list of account addresses
+ */
 module.exports = function(deployer, network, accounts) {
-  let network_id = parseInt(deployer.network_id)
+  const networkId = parseInt(deployer.network_id, 10)
 
-  // Check that it is a network that'ss supported by web3data
-  if ([1,4].indexOf(network_id) < 0) { return }
+  // Check that network is supported by amberdata
+  if ([1, 4].indexOf(networkId) < 0) {
+    console.info(
+      `\n [ Uploader ] \u001B[32mINFO:\u001B[0m Network, '${
+        ETHEREUM_NETWORKS[networkId] ? ETHEREUM_NETWORKS[networkId] : networkId
+      }', not supported by amberdata. Contract ABI not uploaded.\n`
+    )
+    return
+  }
 
   fs.readdir(DEFAULT_BUILD_DIR, (err, files) => {
     for (let i = 1; i < files.length; i++) {
+
+      // Get absolute file path of conract abi and load the api
       const filePath = path.join(DEFAULT_BUILD_DIR, '/', files[i])
       const abi = JSON.parse(fs.readFileSync(filePath))
-      let networks = abi.networks[network_id]
-      if (!isEmpty(networks)) {
-        const payload = getMeta(networks, network_id, accounts[0])
-        uploadFile(filePath, payload)
+
+      /* Check if the networks field is empty. If it is, then contract
+       * was not deployed to a network
+       */
+      if (isEmpty(abi.networks)) {
+        console.log(
+          `\n[ Uploader ] \u001B[32mINFO:\u001B[0m '${
+            files[i]
+          }' might not have been deployed therefore it was skipped by the Uploader`
+        )
+      } else {
+
+        // Get the 'networks' object from the abi, parse data and contruct the payload then upload the file
+        const networks = abi.networks[networkId]
+        const payload = getMeta(networks, networkId, accounts[0])
+        uploadAbi(filePath, payload)
       }
     }
   })
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,24 +1,25 @@
 const fs = require('fs')
 const path = require('path')
-let MIGRATIONS_DIR = path.join(__dirname, '/../../../', 'migrations')
+let MIGRATIONS_DIR = './migrations'//path.join(__dirname, '/../../../', 'migrations')
+const RERUN_MSG = 'To rerun the postinstall script:\n\tnpm explore web3data-deploy -- npm run postinstall'
 let addUploadFile = function() {
 
   if (!fs.existsSync(MIGRATIONS_DIR)) {
-    throw new Error('Unable to locate migrations folder\ncheck that your \'migrations\' folder is in your project root directory')
+    throw new Error(`\u001B[31mUnable to locate migrations folder check that your \'migrations\' folder is in your project root directory\u001B[0m\n${RERUN_MSG}`)
   }
 
-  // Get all files in migrations folder
+  // Iterate through files in the migrations directory
   fs.readdir(MIGRATIONS_DIR, (err, files) => {
     if (err) throw err
-    if (!files.length > 0) throw new Error('migrations folder is empty')
-    const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${files.length + 2}_abi_analytics.js`)
-    const abiSource = fs.readFileSync(ABI_ANALYTICS_PATH)
+    if (!files.length > 0) throw new Error('Migrations folder is empty')
+
+    const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${files.length + 2}-abi-analytics.js`)
+    const abiSource = fs.readFileSync('./x-abi-analytics.js')
     fs.writeFileSync(ABI_ANALYTICS_PATH, abiSource, 'utf-8')
-    console.log(`Added \'${files.length + 2}_abi_analytics\' to \'migrations\' directory`)
 
-
-    console.log("add notice of API Key setup if no found")
-    console.log("link to get amberdata key")
+    console.log(`'${files.length + 2}-abi-analytics' has been added to your 'migrations' directory!`)
+    console.log("Set your api key one of two ways:\n\t1) Run 'export AMBERDATA_API_KEY=<your_api_key>' in terminal\n\t2) Add this line: 'AMBERDATA_API_KEY=<your_api_key>' to your .env file\n")
+    console.log("Need an api key? Go to \u001B[36mhttps://amberdata.io/user/api-keys\u001B[0m to obtain one!\n")
   })
 }
 addUploadFile()
