65b36e2d1c65320306155e4b778e03b11e5b3779
Taylor Dawson <taylorjdawson@gmail.com>
2018-10-15 18:18:38 -0700

    Removed error throwing

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -4,22 +4,27 @@ let MIGRATIONS_DIR = path.join(__dirname, '/../../../', 'migrations')
 const RERUN_MSG = 'To rerun the postinstall script:\n\tnpm explore web3data-deploy -- npm run postinstall'
 let addUploadFile = function() {
 
+  // Check that we can find the migrations folder
   if (!fs.existsSync(MIGRATIONS_DIR)) {
-    throw new Error(`\u001B[31mUnable to locate migrations folder check that your \'migrations\' folder is in your project root directory\u001B[0m\n${RERUN_MSG}`)
+    console.log(`\u001B[31mUnable to locate migrations folder check that your \'migrations\' folder is in your project root directory\u001B[0m\n${RERUN_MSG}`)
+  } else {
+
+    // Iterate through files in the migrations directory
+    fs.readdir(MIGRATIONS_DIR, (err, files) => {
+      if (err) {
+        console.log(`\u001B[31m${err}\u001B[0m\n`)
+      }
+      if (!files.length > 0) {
+        console.log('\u001B[31mMigrations folder is empty\u001B[0m\n')
+      }
+      const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${files.length + 2}-abi-analytics.js`)
+      const abiSource = fs.readFileSync('./x-abi-analytics.js')
+      fs.writeFileSync(ABI_ANALYTICS_PATH, abiSource, 'utf-8')
+
+      console.log(`'${files.length + 2}-abi-analytics' has been added to your 'migrations' directory!`)
+      console.log("Set your api key one of two ways:\n\t1) Run 'export AMBERDATA_API_KEY=<your_api_key>' in terminal\n\t2) Add this line: 'AMBERDATA_API_KEY=<your_api_key>' to your .env file\n")
+      console.log("Need an api key? Go to \u001B[36mhttps://amberdata.io/user/api-keys\u001B[0m to obtain one!\n")
+    })
   }
-
-  // Iterate through files in the migrations directory
-  fs.readdir(MIGRATIONS_DIR, (err, files) => {
-    if (err) throw err
-    if (!files.length > 0) throw new Error('Migrations folder is empty')
-
-    const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${files.length + 2}-abi-analytics.js`)
-    const abiSource = fs.readFileSync('./x-abi-analytics.js')
-    fs.writeFileSync(ABI_ANALYTICS_PATH, abiSource, 'utf-8')
-
-    console.log(`'${files.length + 2}-abi-analytics' has been added to your 'migrations' directory!`)
-    console.log("Set your api key one of two ways:\n\t1) Run 'export AMBERDATA_API_KEY=<your_api_key>' in terminal\n\t2) Add this line: 'AMBERDATA_API_KEY=<your_api_key>' to your .env file\n")
-    console.log("Need an api key? Go to \u001B[36mhttps://amberdata.io/user/api-keys\u001B[0m to obtain one!\n")
-  })
 }
 addUploadFile()
