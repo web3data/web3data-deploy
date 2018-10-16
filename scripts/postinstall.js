4d3bad76c3a6088c192c3edd54dd2cb222ab007f
Taylor Dawson <taylorjdawson@gmail.com>
2018-10-16 15:24:57 -0700

    Fixed file names

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -17,11 +17,12 @@ let addUploadFile = function() {
       if (!files.length > 0) {
         console.log('\u001B[31mMigrations folder is empty\u001B[0m\n')
       }
-      const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${files.length + 2}-abi-analytics.js`)
-      const abiSource = fs.readFileSync('./x-abi-analytics.js')
+      const num_prefix = files.length + 1
+      const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${num_prefix}_abi_analytics.js`)
+      const abiSource = fs.readFileSync('./x_abi_analytics.js')
       fs.writeFileSync(ABI_ANALYTICS_PATH, abiSource, 'utf-8')
 
-      console.log(`'${files.length + 2}-abi-analytics' has been added to your 'migrations' directory!`)
+      console.log(`'${num_prefix}_abi_analytics' has been added to your 'migrations' directory!`)
       console.log("Set your api key one of two ways:\n\t1) Run 'export AMBERDATA_API_KEY=<your_api_key>' in terminal\n\t2) Add this line: 'AMBERDATA_API_KEY=<your_api_key>' to your .env file\n")
       console.log("Need an api key? Go to \u001B[36mhttps://amberdata.io/user/api-keys\u001B[0m to obtain one!\n")
     })
