5c3d5e4c3fef8253dc5ba4907e4306e0ce308ea9
Taylor Dawson <taylorjdawson@gmail.com>
2018-11-29 09:37:04 +0000

    postinstall script deletes pre-existing script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -18,19 +18,21 @@ let addUploadFile = function() {
         console.log('\u001B[31mMigrations folder is empty\u001B[0m\n')
       }
 
-      // Check that the mirgrations script doesn't already exist
-      if(files.join('').match('_abi_analytics')) {
-        const num_prefix = files.length + 1
-        const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${num_prefix}_abi_analytics.js`)
-        const abiSource = fs.readFileSync('./x_abi_analytics.js')
-        fs.writeFileSync(ABI_ANALYTICS_PATH, abiSource, 'utf-8')
+      // Check for pre-existing script and delete it
+      let existing_script = files.join('').match('[0-9]_abi_analytics')
 
-        console.log(`'${num_prefix}_abi_analytics.js' has been added to your 'migrations' directory!`)
-        console.log("Set your api key one of two ways:\n\t1) Run 'export AMBERDATA_API_KEY=<your_api_key>' in terminal\n\t2) Add this line: 'AMBERDATA_API_KEY=<your_api_key>' to your .env file\n")
-        console.log("Need an api key? Go to \u001B[36mhttps://amberdata.io/user/api-keys\u001B[0m to obtain one!\n")
-      } else {
-        console.log(`'X_abi_analytics.js' has already been added to your 'migrations' directory 😉`)
+      if(existing_script) {
+        fs.unlinkSync(path.join(MIGRATIONS_DIR, '/', existing_script[0]))
       }
+
+      const num_prefix = files.length + 1
+      const ABI_ANALYTICS_PATH = path.join(MIGRATIONS_DIR, `${num_prefix}_abi_analytics.js`)
+      const abiSource = fs.readFileSync('./x_abi_analytics.js')
+      fs.writeFileSync(ABI_ANALYTICS_PATH, abiSource, 'utf-8')
+
+      console.log(`'${num_prefix}_abi_analytics.js' has been added to your 'migrations' directory!`)
+      console.log("Set your api key one of two ways:\n\t1) Run 'export AMBERDATA_API_KEY=<your_api_key>' in terminal\n\t2) Add this line: 'AMBERDATA_API_KEY=<your_api_key>' to your .env file\n")
+      console.log("Need an api key? Go to \u001B[36mhttps://amberdata.io/user/api-keys\u001B[0m to obtain one!\n")
     })
   }
 }
